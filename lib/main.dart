ffcba8c54556eada0f5c834dd636c4e28f9ab8a8
Kyaw Thet Aung <110282304+kyawthetaung-dev@users.noreply.github.com>
2024-05-19 07:43:01 +0630

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: FutureBuilder<NoteDatabase>(
-        future:$FloorNoteDatabase.databaseBuilder('note.db').build() ,
+        future:$FloorNoteDatabase.databaseBuilder('note.db').build(),
        builder: (context, data){
         if(data.hasData){
           Get.put(data.data!.noteDao);
